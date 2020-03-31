a0e9740bd6893e9da3626e82bfe07d47c1702c5a
zbodnick <zbodnick@gwmail.gwu.edu>
2020-03-31 14:54:18 -0400

    Added course manager activity, with r ecycler row view

---
+++ SkiMate/app/src/main/java/com/bodnick/skimate/Course.kt
@@ -0,0 +1,14 @@
+package com.bodnick.skimate
+
+data class Course (
+    val name: String,
+    val location: String,
+    val lat: String,
+    val lng: String,
+//    val type: String,
+    val thumbnail: String,
+    val weatherIcon: String,
+    val temp: String,
+    val precipitation: String
+)
+
---
+++ SkiMate/app/src/main/java/com/bodnick/skimate/CourseManagerActivity.kt
@@ -0,0 +1,12 @@
+package com.bodnick.skimate
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class CourseManagerActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_course_manager)
+    }
+}
