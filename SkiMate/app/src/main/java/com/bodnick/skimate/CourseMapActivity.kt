e9b94681a2ab40434f3af46dbeb06e38625c71c1
zbodnick <zbodnick@gwmail.gwu.edu>
2020-04-13 23:24:47 -0400

    Added course placement feature with undo button

--- SkiMate/app/src/main/java/com/bodnick/skimate/CourseMapActivity.kt
+++ SkiMate/app/src/main/java/com/bodnick/skimate/CourseMapActivity.kt
@@ -1,12 +1,16 @@
 package com.bodnick.skimate
 
 import android.content.Context
+import android.content.Intent
 import android.graphics.Bitmap
 import android.graphics.Canvas
 import android.os.Bundle
+import android.util.Log
+import android.widget.ImageButton
 import android.widget.SeekBar
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
+import androidx.core.view.isVisible
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
@@ -16,44 +20,44 @@ import com.google.android.gms.maps.model.*
 class CourseMapActivity : AppCompatActivity(), OnMapReadyCallback {
 
     private lateinit var mMap: GoogleMap
-    private lateinit var resizeSeekbar: SeekBar
     private lateinit var rotateSeekbar: SeekBar
-    private var resizeProgress: Int = 0
-    private var rotateProgress: Int = 0
+    private lateinit var undoButton: ImageButton
     private var rotatingCourse: Boolean = true
+    private var coursePlaced: Boolean = false
     private lateinit var courseOverlay: GroundOverlay
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_course_map)
 
-        resizeSeekbar = findViewById(R.id.resizeSeekBar)
-        rotateSeekbar = findViewById(R.id.rotateSeekBar)
+//        rotateSeekbar = findViewById(R.id.rotateSeekBar)
+        undoButton = findViewById(R.id.undoButton)
+        undoButton.isEnabled = false
 
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         val mapFragment = supportFragmentManager
             .findFragmentById(R.id.map) as SupportMapFragment
         mapFragment.getMapAsync(this)
-
-        rotateSeekbar.setOnSeekBarChangeListener(object :
-            SeekBar.OnSeekBarChangeListener {
-            override fun onProgressChanged(seek: SeekBar, progress: Int, fromUser: Boolean) {
-//                if (!preferencesSaved) {
-//                    restuarantCount.text = "Results (${restaurantSeekbar.progress}):"
-//                } else {
-//                    restuarantCount.text = "Results (${preferences.getInt("numRestaurants",0)}):"
-//                }
-
-                courseOverlay.bearing = progress.toFloat()
-
-            }
-
-            override fun onStartTrackingTouch(seek: SeekBar) {
-            }
-
-            override fun onStopTrackingTouch(seek: SeekBar) {
-            }
-        })
+//
+//        rotateSeekbar.setOnSeekBarChangeListener(object :
+//            SeekBar.OnSeekBarChangeListener {
+//            override fun onProgressChanged(seek: SeekBar, progress: Int, fromUser: Boolean) {
+////                if (!preferencesSaved) {
+////                    restuarantCount.text = "Results (${restaurantSeekbar.progress}):"
+////                } else {
+////                    restuarantCount.text = "Results (${preferences.getInt("numRestaurants",0)}):"
+////                }
+//
+//                courseOverlay.bearing = progress.toFloat()
+//
+//            }
+//
+//            override fun onStartTrackingTouch(seek: SeekBar) {
+//            }
+//
+//            override fun onStopTrackingTouch(seek: SeekBar) {
+//            }
+//        })
 
     }
 
@@ -83,23 +87,41 @@ class CourseMapActivity : AppCompatActivity(), OnMapReadyCallback {
 //            .anchor(0.5f, 0.5f)
 //            .title(name)
 //            .icon(BitmapDescriptorFactory.fromBitmap(R.drawable.ic_course_overlay.toBitmap(this, resizeProgress))))
+        mMap.setOnMapLongClickListener { latLng: LatLng ->
+            Log.d("CourseMapActivity", "Course placed at ${latLng.latitude}, ${latLng.longitude}")
 
-        courseOverlay = mMap.addGroundOverlay (
-            GroundOverlayOptions()
-                .image(BitmapDescriptorFactory.fromBitmap(R.drawable.ic_course_overlay_proper_scale.toBitmap(this, resizeProgress)))
-                .position(course, 25f)
-        )
+            if ( !undoButton.isEnabled ) {
+                courseOverlay = mMap.addGroundOverlay(
+                    GroundOverlayOptions()
+                        .image(
+                            BitmapDescriptorFactory.fromBitmap(
+                                R.drawable.ic_course_overlay_proper_scale.toBitmap(
+                                    this
+                                )
+                            )
+                        )
+                        .position(latLng, 26f)
+                )
+            }
+
+            undoButton.isEnabled = true
+        }
+
+        undoButton.setOnClickListener {
+            courseOverlay.remove()
+            undoButton.isEnabled = false
+        }
 
         val zoomLevel = 16.0f
         mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(course, zoomLevel))
     }
 
-    private fun Int.toBitmap(context: Context, resizeAmount: Int): Bitmap? {
+    private fun Int.toBitmap(context: Context): Bitmap? {
 
         // Retrieve vector asset drawable
         val drawableAsset = ContextCompat.getDrawable(context, this) ?: return null
         drawableAsset.setBounds(0, 0, drawableAsset.intrinsicWidth, drawableAsset.intrinsicHeight)
-        val bitmap = Bitmap.createBitmap(drawableAsset.intrinsicWidth+resizeAmount, drawableAsset.intrinsicHeight+resizeAmount, Bitmap.Config.ARGB_8888)
+        val bitmap = Bitmap.createBitmap(drawableAsset.intrinsicWidth, drawableAsset.intrinsicHeight, Bitmap.Config.ARGB_8888)
 
         // Draw asset to bitmap
         val canvas = Canvas(bitmap)
