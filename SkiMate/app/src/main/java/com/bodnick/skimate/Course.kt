f975d5ee6994e4304f9602862404950ef04241fc
zbodnick <zbodnick@gwmail.gwu.edu>
2020-04-15 21:40:40 -0400

    Added empty constructor to Course class

--- SkiMate/app/src/main/java/com/bodnick/skimate/Course.kt
+++ SkiMate/app/src/main/java/com/bodnick/skimate/Course.kt
@@ -1,5 +1,7 @@
 package com.bodnick.skimate
 
+import java.io.Serializable
+
 data class Course (
     var name: String?,
     var location: String,
@@ -10,4 +12,8 @@ data class Course (
     var precipitation: String?,
     var wind: String?,
     var bearing: String
-)
+) : Serializable {
+    // Required by Firebase to cast to a custom object
+    // Calls the primary constructor with default arguments
+    constructor() : this("","","","", "", "", "", "", "")
+}
